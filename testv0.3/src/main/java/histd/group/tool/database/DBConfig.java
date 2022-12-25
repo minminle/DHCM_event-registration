993e48ee835b9038059999e105628656f2b0367a
zhen30 <113868091+zhen30@users.noreply.github.com>
2022-12-25 16:56:27 +0800

    Update DBConfig.java

--- testv0.3/src/main/java/histd/group/tool/database/DBConfig.java
+++ testv0.3/src/main/java/histd/group/tool/database/DBConfig.java
@@ -6,13 +6,13 @@ public class DBConfig {
 	
 	public String FilePath(){
 		//宜蓁
-		 //String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\dhcm-practice\\src\\main\\webapp\\dhcm-practice.accdb";
+		 String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\DHCM_take-a-leave\\testv0.3\\src\\main\\webapp\\dhcm.accdb";
 		 
 		//合佳
 		 //String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\test_11-23\\src\\main\\webapp\\dhcm-practice.accdb";
 		 
 		//嘉珉
-		 String DBPath="C:\\Users\\cindy\\Desktop\\DHCM_take-a-leave\\testv0.3\\src\\main\\webapp\\dhcm.accdb";
+		 //String DBPath="C:\\Users\\cindy\\Desktop\\DHCM_take-a-leave\\testv0.3\\src\\main\\webapp\\dhcm.accdb";
 	   return DBPath;		
 	}
 }
