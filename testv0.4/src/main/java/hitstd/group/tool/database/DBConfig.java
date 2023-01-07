3b4338214c13988fe9b4e30389be311465b61abb
zhen30 <113868091+zhen30@users.noreply.github.com>
2023-01-07 18:55:53 +0800

    Update DBConfig.java

--- testv0.4/src/main/java/hitstd/group/tool/database/DBConfig.java
+++ testv0.4/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -5,9 +5,14 @@ public class DBConfig {
 	}
 	
 	public String FilePath(){
-	   //宜蓁資料庫
+		 //嘉珉資料庫
+		 //String DBPath="";
+		 
+		 //合佳資料庫
+		 //String DBPath="";
+		 
+		 //宜蓁資料庫
 		 String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\DHCM_take-a-leave\\testv0.4\\src\\main\\webapp\\eventA.accdb";
-	   
 	return DBPath;		
 	}
 }
