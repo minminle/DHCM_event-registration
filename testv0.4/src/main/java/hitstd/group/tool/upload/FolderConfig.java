0d158ce92cc247688e288f149540a068975ae661
zhen30 <113868091+zhen30@users.noreply.github.com>
2023-01-10 19:51:40 +0800

    0110

--- testv0.4/src/main/java/hitstd/group/tool/upload/FolderConfig.java
+++ testv0.4/src/main/java/hitstd/group/tool/upload/FolderConfig.java
@@ -6,24 +6,24 @@ public class FolderConfig {
 	
 	public String FilePath(){
 	   //宜蓁
-		//String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\DHCM_take-a-leave\\testv0.4\\src\\main\\webapp\\img\\";
+		String DBPath="C:\\Users\\s0979\\OneDrive\\文件\\GitHub\\DHCM_take-a-leave\\testv0.4\\src\\main\\webapp\\img\\";
 	   
 	   //合佳
 		//String DBPath="/Users/sunhochia/Documents/GitHub/DHCM_event-registration/testv0.4/src/main/webapp/img/";
 		
 	   //嘉珉
-		String DBPath="C:/Users/cindy/Desktop/DHCM_event-registration/testv0.4/src/main/webapp/img/";
+		//String DBPath="C:/Users/cindy/Desktop/DHCM_event-registration/testv0.4/src/main/webapp/img/";
 
 	}
 	public String WebsiteRelativeFilePath(){
 		//宜蓁
-		 //String Path="img/";
+		 String Path="img/";
 		
 		//合佳
 		 //String Path="img/";
 			 
 		//嘉珉
-		 String Path="img";
+		 //String Path="img";
 		   return Path;		
 		}
 }
